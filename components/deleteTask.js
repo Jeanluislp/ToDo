606af4cac8bfe0a0308bf07d5e307bb9b41624d4
JeanLuislp <jeanluislp@gmail.com>
2022-05-19 03:22:54 -0500

    arreglando error

--- components/deleteTask.js
+++ components/deleteTask.js
@@ -7,7 +7,7 @@ const createDeleteIcon = (id) => {
 };
 const deleteParent = (id) => {
   const tasks = document.querySelector("[data-tasks]");
-  //tasks.innerHTML = "";
+  tasks.innerHTML = "";
   const taskList = JSON.parse(localStorage.getItem("ToDoTaks"));
   const index = taskList.findIndex((item) => item.id === id);
   console.log(index);
